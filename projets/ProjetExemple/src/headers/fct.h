58ced5320c4cb786d6db6621be8b49ff3edd29d7
Yann <yann55446@gmail.com>
2025-02-17 19:29:50 +0100

    Add files via upload

---
+++ projets/ProjetExemple/src/headers/fct.h
@@ -0,0 +1,9 @@
+#ifndef _FCT_H_
+#define _FTC_H_
+
+typedef enum{
+FAUX,
+VRAI
+}booleen;
+
+#endif //_FCT_H_
---
+++ projets/ProjetExemple/src/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"headers.h"
+int main(int argc, char *argv[])
+{
+printf("Modele main.c\n");
+return 0;
+}
