235bad4c2d3881bcbb19f956df83ac240ebcf889
yaya66659 <yann55446@gmail.com>
2025-05-06 01:13:41 +0200

    Sauvegarde locale avant rebase

---
+++ projets/yannFinal000/src/fct.c
@@ -0,0 +1,27 @@
+// -*- coding: utf-8 -*-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <stdbool.h>
+
+#include "headers/fct.h"
+
+void videBuffer(void)
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ; /*Vide le buffer clavier*/
+}
+
+void activeAffichageUTF8DansLaConsole(bool activerUTF8pourLaSaisie)
+{
+
+    SetConsoleOutputCP(CP_UTF8); /* Force l'affichage correct des caractères UTF-8*/
+
+    if (activerUTF8pourLaSaisie)
+    {
+        SetConsoleCP(CP_UTF8); /* Force l'affichage UTF-8* Pour la saisie*/
+    }
+}
---
+++ projets/yannFinal000/src/headers/fct.h
@@ -0,0 +1,12 @@
+#ifndef FCT_05052025_234712_yannFinal000_H
+#define FCT_05052025_234712_yannFinal000_H
+
+#include <stdbool.h>
+
+#define BUFFER_TEXTE_MAX 256
+#define BUFFER_CHEMIN_MAX 512
+
+void videBuffer(void);
+void activeAffichageUTF8DansLaConsole(bool activerUTF8pourLaSaisie);
+
+#endif /*FCT_05052025_234712_yannFinal000_H*/
